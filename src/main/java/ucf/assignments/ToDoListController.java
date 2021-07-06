11dabf56256e20d980aafd7ebf6ee016d64964a7
Ursula Shaw <ursula.e.shaw@gmail.com>
2021-07-05 23:40:08 -0400

    ToDoList class created copyright stuff git.ignore, fxml, plantUML update

--- src/main/java/ucf/assignments/ToDoListController.java
+++ src/main/java/ucf/assignments/ToDoListController.java
@@ -1,4 +1,35 @@
 package ucf.assignments;
 
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Ursula Shaw
+ */
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import javax.swing.*;
+
 public class ToDoListController {
+
+    public void addList(ActionEvent actionEvent) {
+    }
+
+    public void showAll(ActionEvent actionEvent) {
+    }
+
+    public void showUncompleted(ActionEvent actionEvent) {
+    }
+
+    public void showCompleted(ActionEvent actionEvent) {
+    }
+
+    public void saveAllLists(ActionEvent actionEvent) {
+    }
+
+    public void saveList(ActionEvent actionEvent) {
+    }
+
+    public void addItem(ActionEvent actionEvent) {
+    }
 }
